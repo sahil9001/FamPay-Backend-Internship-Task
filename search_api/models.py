ed1268d15a35fba4c2c16f11c56408cf3af643bf
Sahil Silare <sahilsilare@gmail.com>
2021-08-05 19:26:06 +0530

    feat: Added Project base and functionalities.
    - Added Dockerfile for dockerizing the application.
    - Added CRON jobs to schedule the API call every 1 minute.
    - Added Search Model to store the results in the cached database

---
+++ search_api/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Videos(models.Model):
+    vid_id = models.CharField(max_length=100, primary_key=True)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    published_at = models.DateTimeField()
+    thumbnailsUrls = models.URLField(max_length=1000)
+    channel_id = models.CharField(max_length=100)
+    channel_title = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.title
